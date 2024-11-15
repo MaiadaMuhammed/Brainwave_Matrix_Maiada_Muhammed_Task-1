0278d6cee272d0d71c56113108f2c58aa76cfdf4
MAIADA_MUHAMMED <166024947+MaiadaMuhammed@users.noreply.github.com>
2024-11-15 16:55:56 +0200

    Delete Landing page directory

--- Landing page/script.js
+++
@@ -1,2 +0,0 @@
-// JavaScript can be used for dynamic features like smooth scrolling, form validation, or handling user interactions.
-// As this is a simple landing page, no script is added here for now.

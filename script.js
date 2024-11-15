79914fb7af54b3e0fd57f7dbbbcc489031c88a7b
MAIADA_MUHAMMED <166024947+MaiadaMuhammed@users.noreply.github.com>
2024-11-15 16:56:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+// JavaScript can be used for dynamic features like smooth scrolling, form validation, or handling user interactions.
+// As this is a simple landing page, no script is added here for now.
